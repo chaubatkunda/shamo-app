f6341fbb017b90155d4e3ef5cc44ede77b807af5
chaubatkunda <maniklusi31@gmail.com>
2023-08-16 09:27:03 +0700

    create and configure shamo_app

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'theme.dart';
+import 'pages/splash_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashPage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Hello World',
+          style: secondaryTextStyle,
+        ),
+      ),
+    );
+  }
+}
